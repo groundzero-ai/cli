4d444b99a111688f67bd03ddb26f98f3bdb40c52
Eric Lee <hyericlee@gmail.com>
2025-11-30 21:21:56 +0800

    Cleaned up unused files

--- src/core/install/index.ts
+++
@@ -1,4 +0,0 @@
-export { runInstallPipeline } from './install-pipeline.js';
-export type { InstallPipelineOptions, InstallPipelineResult } from './install-pipeline.js';
-export { runBulkInstallPipeline } from './bulk-install-pipeline.js';
-
